daca666cba6e32dfc4285450b6352b31d296fa5b
luwam <lwamyohannes2018@gmail.com>
2023-08-10 00:54:46 -0600

    composer-char-counter.js and new-tweet.css added

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,5 @@
+console.log("The composer-char-counter file has been loaded.");
+const $(document).ready(function() {
+  // --- our code goes here ---
+  console.log("The composer-char-counter file has been loaded.");
+});
