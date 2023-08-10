e3b0fdfa72a0a8f9d032837964a8a92da6ee8d06
luwam <lwamyohannes2018@gmail.com>
2023-08-10 15:54:09 -0600

    textarea and button modified

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,8 @@
-console.log("The composer-char-counter file has been loaded.");
-const $(document).ready(function() {
+//console.log("The composer-char-counter file has been loaded.");
+ $(document).ready(function() {
   // --- our code goes here ---
   console.log("The composer-char-counter file has been loaded.");
+  $("#tweet-text").on("input", () => {
+    console.log("input");
+  });
 });
