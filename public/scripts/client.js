04e290e273ee5cd38ae072a98415b3d1548c13c9
luwam <lwamyohannes2018@gmail.com>
2023-08-14 13:49:43 -0600

    main tag modified

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,79 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
+};
 
+const $tweet = createTweetElement(tweetData);
+
+// Test / driver code (temporary)
+console.log($tweet); // to see what it looks like
+$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
+
+const renderTweets = function(tweets) {
+// loops through tweets
+// calls createTweetElement for each tweet
+// takes return value and appends it to the tweets container
+};
+
+const createTweetElement = function(tweet) {
+  let $tweet = $(`<article class="tweet">
+  <header>
+    <div class="user-info">
+      <img src="${tweet.user.avatars}" alt="User Avatar">
+      <h3 class="username">${tweet.user.name}</h3>
+    </div>
+    <span class="handle">${tweet.user.handle}</span>
+  </header>
+  <div class="tweet-content">
+    <p>${escape(tweet.content.text)}</p>
+  </div>
+  <footer>
+    <span class="timeago">${timeago.format(tweet.created_at)}</span>
+    <div class="icons">
+    <i class="fa-solid fa-flag"></i>
+    <i class="fa-solid fa-retweet"></i>
+    <i class="fa-solid fa-heart"></i>
+    </div>
+  </footer>
+</article>
+`);/* Your code for creating the tweet element */
+  // ...
+
+  return $tweet;
+};
+
+renderTweets(data);
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,25 @@
 //console.log("The composer-char-counter file has been loaded.");
- $(document).ready(function() {
+$(document).ready(function () {
   // --- our code goes here ---
-  console.log("The composer-char-counter file has been loaded.");
-  $("#tweet-text").on("input", () => {
-    console.log("input");
+  
+  // Register an event handler for the textarea element
+  $(".new-tweet").find("textarea").on("input", function() {
+    const maxLength = 140;
+    const currentLength = $(this).val().length;
+    const remainingChars = maxLength - currentLength;
+
+    // Update the counter value
+    $(this).closest(".new-tweet").find(".counter").text(remainingChars);
+
+    // Change the counter color if the limit is exceeded
+    if (remainingChars < 0) {
+      $(this).closest(".new-tweet").find(".counter").addClass("invalid");
+    } else {
+      $(this).closest(".new-tweet").find(".counter").removeClass("invalid");
+    }
   });
 });
+
+
+
+
