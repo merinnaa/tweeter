0fb89e92e19540314455292853aee2f78967dfca
luwam <lwamyohannes2018@gmail.com>
2023-08-21 19:59:31 -0600

    fix bugs after reviewing with mentor

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,60 +6,32 @@
 
 $(document).ready(function () {
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
 
   const renderTweets = function (tweets) {
 
     // loops through tweets
-    let container = $('#tweets-container').empty();
-
-    tweets.forEach((tweet) => {
-
-      // calls createTweetElement for each tweet
-      const $tweetElement = createTweetElement(tweet);
-
-      // takes return value and appends it to the tweets container
-      container.prepend($tweetElement);
-    });
-
+    $('#tweets-container').empty();
+    for (const tweet of tweets) {
+      createTweetElement(tweet);
+    }
   };
 
+
+
+
   const createTweetElement = function (tweet) {
+    console.log("text");
     let $tweet = `<article class="tweet-container">
   <header class="tweet-header">
-    <div class="user-info">
-      <img src="${tweet.user.avatars}" alt="User Avatar">
-      <h3 class="username">${tweet.user.name}</h3>
+    <div class="tweet-avator-box">
+      <img class="tweet-avator" src="${tweet.user.avatars}" alt="User Avatar">
+      <h5 class="tweet-username">${tweet.user.name}</h5>
     </div>
     <span class="handle">${tweet.user.handle}</span>
   </header>
   <div class="tweet-content">
   <!-- Apply XSS-escaped content to the <p> element -->
-   <p>${escape(tweet.content.text)}</p>
+   <p>${tweet.content.text}</p>
   </div>
   <footer class="tweet-footer">
   <span class="timeago">${timeago.format(tweet.created_at)}</span>
@@ -72,15 +44,15 @@ $(document).ready(function () {
 </article>
 `;
 
-    return $tweet;
+    $('#tweets-container').prepend($tweet);
   };
 
   $("form").on("submit", function (event) {
     event.preventDefault();
     // Get the tweet content from the form
-    const tweetContent = $(this).find("textarea[name='text']").val();
+    const tweetContent = $("#tweet-text").val();
     const $errorMessage = $(".error-message"); // Select the error message element
-
+    console.log(tweetContent);
     // Hide the error message before validation
     $errorMessage.hide();
 
@@ -93,35 +65,25 @@ $(document).ready(function () {
 
     if (tweetContent.length > 140) {
       // Display an error message if tweet content exceeds the character limit
-      
+
       $errorMessage.text("Tweet content is too long. Maximum 140 characters allowed.").slideDown();
 
       return; // Stop further execution
     }
-    const dataForm = $(this).serialize();
-    $.post('/tweets', dataForm)
-      .then((response) => {
-        loadTweets();
-        // Call loadTweets to fetch updated tweets from the server
-        loadTweets();
-        // Clear the form
-        $(this)[0].reset();
-        // Handle success response from the server
-        console.log('success:', response);
-      });
+    //const dataForm = $(this).serialize();
+    
+    $.ajax({
+      method: 'POST',
+      url: '/tweets',
+      data: $(this).serialize(),
+    }).then(loadTweets);
   });
 
   const loadTweets = function () {
-    $.get('/tweets')
-      .then((response) => {
-        
-        // Call renderTweets with the received JSON response
-        renderTweets(response);
-        $('#tweets-container').prepend(createTweetElement(response[response.length - 1]));
-      });
-
+    $("#tweet-text").val("");
+    $.ajax('/tweets',
+      { method: 'GET' }).then(renderTweets);
   };
 
   loadTweets();
-  renderTweets(data);
 });
